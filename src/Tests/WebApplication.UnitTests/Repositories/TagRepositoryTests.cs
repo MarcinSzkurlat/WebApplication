4188da217cb802dfdb41c8e34468960d776d2c81
MarcinSzkurlat <szkurlat.martin@gmail.com>
2024-03-29 13:59:08 +0100

    Added unit tests

---
+++ src/Tests/WebApplication.UnitTests/Repositories/TagRepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationAPI;
+using WebApplicationAPI.Models;
+using WebApplicationAPI.Repositories;
+
+namespace WebApplication.UnitTests.Repositories
+{
+    public class TagRepositoryTests
+    {
+        private DbContextOptions<WebApplicationDbContext> _options;
+        private const int PageNumber = 1;
+        private const int PageSize = 10;
+        private readonly List<Tag> _tags = new()
+        {
+            new Tag(){Id = 1, Name = "Tag1", Count = 30, Percentage = 30},
+            new Tag(){Id = 2, Name = "Tag2", Count = 20, Percentage = 20},
+            new Tag(){Id = 3, Name = "Tag3", Count = 50, Percentage = 50}
+        };
+        
+        [SetUp]
+        public async Task Setup()
+        {
+            _options = new DbContextOptionsBuilder<WebApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            await using (var dbContext = new WebApplicationDbContext(_options))
+            {
+                await dbContext.Tags.AddRangeAsync(_tags);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        [TearDown]
+        public async Task Teardown()
+        {
+            await using (var dbContext = new WebApplicationDbContext(_options))
+            {
+                dbContext.Tags.RemoveRange(_tags);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        [TestCase(SortBy.Name, SortDirection.ASC, 1,3)]
+        [TestCase(SortBy.Name, SortDirection.DESC, 3,1)]
+        [TestCase(SortBy.Percentage, SortDirection.ASC, 2,3)]
+        [TestCase(SortBy.Percentage, SortDirection.DESC, 3,2)]
+        public async Task GetTagsAsync_ReturnsSortedTags_AppropriateParametersPassed(SortBy sortBy, SortDirection sortDirection, int expectedFirst, int expectedLast)
+        {
+            await using (var dbContext = new WebApplicationDbContext(_options))
+            {
+                var tagRepository = new TagRepository(dbContext);
+                var result = await tagRepository.GetTagsAsync(PageNumber, PageSize, sortBy, sortDirection);
+
+                Assert.AreEqual(expectedFirst, result.First().Id);
+                Assert.AreEqual(expectedLast, result.Last().Id);
+            }
+        }
+
+        [Test]
+        public async Task GetTagsCountAsync_ReturnsTagsCount_WhenTrigger()
+        {
+            await using (var dbContext = new WebApplicationDbContext(_options))
+            {
+                var tagRepository = new TagRepository(dbContext);
+                var result = await tagRepository.GetTagsCountAsync();
+
+                Assert.AreEqual(3, result);
+            }
+        }
+    }
+}
---
+++ src/Tests/WebApplication.UnitTests/Services/TagServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using WebApplicationAPI.Dtos;
+using WebApplicationAPI.Interfaces.Repositories;
+using WebApplicationAPI.Models;
+using WebApplicationAPI.Services;
+
+namespace WebApplication.UnitTests.Services
+{
+    public class TagServiceTests
+    {
+        private Mock<IMapper> _mockMapper;
+        private Mock<ITagRepository> _mockTagRepository;
+        private readonly SortBy SortBy = SortBy.Percentage;
+        private readonly SortDirection SortDirection = SortDirection.DESC;
+        private const int PageSize = 10;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockMapper = new Mock<IMapper>();
+            _mockTagRepository = new Mock<ITagRepository>();
+        }
+
+        [Test]
+        public async Task GetTagsAsync_ReturnsPaginatedItemsOfTagDto_AllParametersPassed()
+        {
+            const int pageNumber = 1;
+            const int totalPages = 1;
+
+            var expectedTags = new List<Tag>()
+            {
+                new Tag() { Id = 1, Name = "Tag1", Count = 10, Percentage = 50 },
+                new Tag() { Id = 2, Name = "Tag2", Count = 10, Percentage = 50 }
+            };
+
+            var expectedTagDtos = new List<TagDto>()
+            {
+                new TagDto("Tag1", 10, 50),
+                new TagDto("Tag2", 10, 50)
+
+            };
+
+            var expectedResult = new PaginatedItems<IEnumerable<TagDto>>(expectedTagDtos, pageNumber, totalPages);
+
+            _mockTagRepository.Setup(x => x.GetTagsAsync(pageNumber, PageSize, SortBy, SortDirection))
+                .ReturnsAsync(expectedTags);
+            _mockTagRepository.Setup(x => x.GetTagsCountAsync()).ReturnsAsync(expectedTags.Count);
+            _mockMapper.Setup(x => x.Map<IEnumerable<TagDto>>(expectedTags)).Returns(expectedTagDtos);
+
+            var tagService = new TagService(_mockTagRepository.Object, _mockMapper.Object);
+
+
+            var result = await tagService.GetTagsAsync(pageNumber, PageSize, SortBy, SortDirection);
+
+
+            Assert.NotNull(result.Items);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public async Task GetTagsAsync_ThrowBadHttpRequestException_PageNumberIsZero()
+        {
+            const int pageNumber = 0;
+
+            var tagService = new TagService(_mockTagRepository.Object, _mockMapper.Object);
+
+
+            Assert.That(async () => await tagService.GetTagsAsync(pageNumber, PageSize, SortBy, SortDirection), Throws.TypeOf<BadHttpRequestException>());
+        }
+
+        [Test]
+        public async Task DeleteTagsAsync_DeletesTagsFromRepository_WhenTrigger()
+        {
+            _mockTagRepository.Setup(x => x.DeleteTagsAsync()).Returns(Task.CompletedTask);
+
+            var tagService = new TagService(_mockTagRepository.Object, _mockMapper.Object);
+
+
+            await tagService.DeleteTagsAsync();
+
+
+            _mockTagRepository.Verify(x => x.DeleteTagsAsync(), Times.Once);
+        }
+    }
+}
